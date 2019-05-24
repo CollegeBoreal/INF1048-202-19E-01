9a89c885577a1486b4b18aa3d8ff1a7f860926d3
Setra <setrar@valiha.com>
2019-05-24 17:04:02 -0400

    .

---
+++ 4.Theme/b300098957/src/app/@theme/theme.module.ts
@@ -0,0 +1,27 @@
+import {ModuleWithProviders, NgModule} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {NbThemeModule} from '@nebular/theme';
+
+const BASE_MODULES = [CommonModule, FormsModule, ReactiveFormsModule];
+
+const NB_MODULES = [
+];
+
+const NB_THEME_PROVIDERS = [
+  ...NbThemeModule.forRoot({ name: 'default' }).providers
+];
+
+@NgModule({
+  declarations: [],
+  imports: [...BASE_MODULES, ...NB_MODULES],
+  exports: [...BASE_MODULES, ...NB_MODULES],
+})
+export class ThemeModule {
+  static forRoot(): ModuleWithProviders {
+    return <ModuleWithProviders> {
+      ngModule: ThemeModule,
+      providers: [...NB_THEME_PROVIDERS],
+    };
+  }
+}
