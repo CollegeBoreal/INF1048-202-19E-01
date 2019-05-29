6b56024732be4af1679021f08964d8f1490ab4e2
Ngandu Muana S Mwamba (Live@EDU) <syngandu@gmail.com>
2019-05-29 12:19:46 -0400

    :x:

---
+++ 4.Theme/b910002000/src/app/pages/page-menu.ts
@@ -0,0 +1,15 @@
+import {NbMenuItem} from "@nebular/theme";
+import {group} from "@angular/animations";
+
+export  const MENU_ITEMS: NbMenuItem[] = [
+  {
+    title: 'Dashboard',
+    icon: 'nb-home',
+    link: '/pages/dashboard',
+    home: true,
+  },
+  {
+    title: 'FEATURES',
+    group: true,
+  }
+];
