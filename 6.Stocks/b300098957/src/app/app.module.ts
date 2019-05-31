fb5fcdc7ff504528cad6aa5e01a57f74a0373ae2
SetraR <setrar@valiha.com>
2019-05-31 10:22:06 -0400

    .

--- 6.Stocks/b300098957/src/app/app.module.ts
+++ 6.Stocks/b300098957/src/app/app.module.ts
@@ -1,29 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import { AppComponent } from './app.component';
-import {StocksService} from './services/stocks.service';
-import {HttpClientModule} from '@angular/common/http';
-import { SummaryComponent } from './components/summary/summary.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { ManageComponent } from './components/manage/manage.component';
-import {FormsModule} from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SummaryComponent,
-    DashboardComponent,
-    ManageComponent
-  ],
+  declarations: [],
   imports: [
-    BrowserModule,
-    HttpClientModule,
-    FormsModule
-  ],
-  providers: [
-    StocksService
-  ],
-  bootstrap: [AppComponent]
+    CommonModule,
+    AppRoutingModule
+  ]
 })
 export class AppModule { }
