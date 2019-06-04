7307c4a9f397f82f6319982bb17a7b2ba292ca68
SetraR <setrar@valiha.com>
2019-06-03 20:35:27 -0400

    adding metric accessors

--- 7.Datacenter/b300098957/src/app/metric/metric.component.ts
+++ 7.Datacenter/b300098957/src/app/metric/metric.component.ts
@@ -9,8 +9,35 @@ export class MetricComponent implements OnInit {
 
   @Input() title: string = '';
   @Input() description: string = '';
-  @Input('used') value: number = 0;
-  @Input('available') max: number = 100;
+
+  // --- USING ACCESSORS ---- START
+  private _value: number = 0;
+  private _max: number = 100;
+
+  @Input('used')
+  set value(value: number) {
+    if (isNaN(value)) {
+      value = 0;
+    }
+    this._value = value;
+  }
+
+  get value(): number {
+    return this._value;
+  }
+
+  @Input('available')
+  set max(max: number) {
+    if (isNaN(max)) {
+      max = 100;
+    }
+    this._max = max;
+  }
+
+  get max(): number {
+    return this._max;
+  }
+  // --- USING ACCESSORS ---- END
 
   constructor() {
   }
