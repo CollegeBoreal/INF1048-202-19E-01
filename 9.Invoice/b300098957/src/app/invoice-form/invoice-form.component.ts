a3dc34ca15b8f30207b8ccf094a04d34e3d4857b
SetraR <setrar@valiha.com>
2019-06-14 10:47:39 -0400

    Before Validation

--- 9.Invoice/b300098957/src/app/invoice-form/invoice-form.component.ts
+++ 9.Invoice/b300098957/src/app/invoice-form/invoice-form.component.ts
@@ -4,6 +4,7 @@ import {ActivatedRoute, Params, Router} from '@angular/router';
 import {CustomersService, InvoicesService, Customer, Invoice} from '../services';
 import {combineLatest} from 'rxjs';
 import {map} from 'rxjs/operators';
+import {HoursValidator} from '../validators/hours.validator';
 
 @Component({
   selector: 'app-invoice-form',
@@ -27,70 +28,77 @@ export class InvoiceFormComponent implements OnInit {
   }
 
   ngOnInit() {
+
     this.invoiceForm = this.formBuilder.group({
       id: [''],
       service: ['', Validators.required],
       customerId: ['', Validators.required],
       rate: ['', Validators.required],
-      hours: ['', [Validators.required]],
+      hours: ['', [Validators.required, HoursValidator]],
       date: ['', Validators.required],
       paid: ['']
     });
 
-    this.customersService.query<Array<Customer>>().subscribe(customers => {
-      this.customers = customers;
-    });
+    this.customersService.query<Array<Customer>>()
+      .subscribe(customers => {
+        this.customers = customers;
+      });
 
     this.route.params.pipe(
       map((params: Params) => params.invoiceId)
     ).subscribe(invoiceId => {
       if (invoiceId) {
-        this.invoicesService.get<Invoice>(invoiceId).subscribe(invoice => {
-          this.invoiceForm.setValue(invoice);
-          this.invoice = invoice;
-        });
+        this.invoicesService.get<Invoice>(invoiceId)
+          .subscribe(invoice => {
+            this.invoiceForm.setValue(invoice);
+            this.invoice = invoice;
+          });
       } else {
         this.invoice = new Invoice();
       }
     });
 
-    combineLatest(
-      this.invoiceForm.get('rate').valueChanges,
-      this.invoiceForm.get('hours').valueChanges
+    combineLatest([
+        this.invoiceForm.get('rate').valueChanges,
+        this.invoiceForm.get('hours').valueChanges
+      ]
     ).subscribe(([rate = 0, hours = 0]) => {
       this.total = rate * hours;
     });
+
   }
 
   save() {
     if (this.invoice.id) {
-      this.invoicesService.update<Invoice>(this.invoice.id, this.invoiceForm.value).subscribe(response => {
-        this.viewInvoice(response.id);
-      });
+      this.invoicesService.update<Invoice>(this.invoice.id, this.invoiceForm.value)
+        .subscribe(response => {
+          this.viewInvoice(response.id);
+        });
     } else {
-      this.invoicesService.create<Invoice>(this.invoiceForm.value).subscribe(response => {
-        this.viewInvoice(response.id);
-      });
+      this.invoicesService.create<Invoice>(this.invoiceForm.value)
+        .subscribe(response => {
+          this.viewInvoice(response.id);
+        });
     }
   }
 
   delete() {
-/*
-    this.dialogService.openConfirm({
-      message: 'Are you sure you want to delete this invoice?',
-      title: 'Confirm',
-      acceptButton: 'Delete'
-    }).afterClosed().subscribe((accept: boolean) => {
-      if (accept) {
-        this.loadingService.register('invoice');
-        this.invoicesService.delete(this.invoice.id).subscribe(response => {
-          this.loadingService.resolve('invoice');
-          this.invoice.id = null;
-          this.cancel();
+    /*
+        this.dialogService.openConfirm({
+          message: 'Are you sure you want to delete this invoice?',
+          title: 'Confirm',
+          acceptButton: 'Delete'
+        }).afterClosed().subscribe((accept: boolean) => {
+          if (accept) {
+            this.loadingService.register('invoice');
+            this.invoicesService.delete(this.invoice.id).subscribe(response => {
+              this.loadingService.resolve('invoice');
+              this.invoice.id = null;
+              this.cancel();
+            });
+          }
         });
-      }
-    });
-*/
+    */
   }
 
   cancel() {
